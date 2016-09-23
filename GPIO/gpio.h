cd0217f13d24a1a91c3833e8a57fe34a4057b704
sunqp <sunqp@meixing.com>
2016-09-23 15:28:10 +0800

    add mmap gpio method

--- GPIO/gpio.h
+++ GPIO/gpio.h
@@ -28,10 +28,6 @@
 #ifndef __GPIO_H__
 #define __GPIO_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 
 /* GPIO value */
 #define GPIO_HIGH     1
@@ -50,91 +46,14 @@ extern "C" {
 #define GPIO_ERROR_WRITE       -3
 #define GPIO_ERROR_DIRECTION   -4
 
+template <typename Method>
+class GPIOHost : public Method
+{
+};
 
-/**
- * Read specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @return integer value of GPIO if success.
- *  return negative integer error code if fail.
- */
-int  gpio_read(int pin_number);
+class GPIOMmap;
+class GPIOSys;
 
-
-
-/**
- * Write specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @param value could be GPIO_HIGH or GPIO_LOW
- *
- * @return 1 if success.
- *  return negative integer error code if fail.
- */
-int  gpio_write(int pin_number, int value);
-
-
-/**
- * set direction of specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @param direction could be GPIO_IN or GPIO_OUT, GPIO_OUT_HIGH, GPIO_OUT_LOW
- *
- * @return 1 if success.
- *  return negative integer error code if fail.
- */
-int  gpio_set_direction(int pin_number, int direction);
-
-/**
- * get direction of specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @return positive integer direction code if success.
- *  return negative integer error code if fail.
- */
-int  gpio_get_direction(int pin_number);
-
-
-/**
- * export specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @return 1 if success.
- *  return negative integer error code if fail.
- */
-int gpio_export(int pin_number);
-
-
-/**
- * unexport specified GPIO pin
- *
- * @param pin_number specified the pin
- *
- * @return 1 if success.
- *  return negative integer error code if fail.
- */
-int gpio_unexport(int pin_number);
-
-/**
- * Return 1 if specified GPIO pin is exported
- *
- * @param pin_number specified the pin
- *
- * @return 1 if GPIO pin is exported.
- *  return 0 if GPIO pin is not exported
- */
-int gpio_is_exported(int pin_number);
-
-
-
-#ifdef __cplusplus
-}
-#endif
-
+typedef GPIOHost<GPIOMmap> GPIO;
 
 #endif /* __GPIO_H__ */
